290dc279346eab40d5af323154ffbe154b02408a
Aleksa <axela.01@gmail.com>
2015-12-26 12:12:39 +0100

    Modal dialog validation is done.. Left to validate accordian forms

--- InsuranceApp/app/components/insurance/insured-modal/insured-modal.contoller.js
+++ InsuranceApp/app/components/insurance/insured-modal/insured-modal.contoller.js
@@ -5,17 +5,19 @@
     .module('insurance-app.insurance')
     .controller('InsuranceModalController', InsuranceModalController);
 
-  InsuranceModalController.$inject = ['$uibModalInstance','user'];
+  InsuranceModalController.$inject = ['$uibModalInstance', 'user'];
 
-  function InsuranceModalController($uibModalInstance,user) {
+  function InsuranceModalController($uibModalInstance, user) {
     var imc = this;
 
     imc.cancel = cancel;
     imc.save = save;
     imc.user = user;
+    imc.validateUser = validateUser;
 
 
-    function save(){
+
+    function save() {
       $uibModalInstance.close(imc.user);
     }
 
@@ -23,5 +25,25 @@
       $uibModalInstance.dismiss();
     }
 
+    function validateUser() {
+      if (imc.stateForm.$valid) {
+        imc.stateForm.$setUntouched();
+        save();
+
+      } else {
+        touchControlls();
+      }
+    }
+
+    function touchControlls() {
+      //Prođi kroz sve propertie stateForm objekta
+      angular.forEach(imc.stateForm, function(value, key) {
+        //Pronađi propertie čiji naziv počinje sa "input"
+        if (key.indexOf("input") === 0) {
+          //"Dodirni" polje
+          value.$setTouched();
+        }
+      });
+    }
   }
 })();
--- InsuranceApp/app/components/insurance/owner-modal/owner-modal.controller.js
+++ InsuranceApp/app/components/insurance/owner-modal/owner-modal.controller.js
@@ -8,19 +8,41 @@
   OwnerModalController.$inject = ['$uibModalInstance','owner'];
 
   function OwnerModalController($uibModalInstance,owner) {
-    var imc = this;
+    var omc = this;
 
-    imc.cancel = cancel;
-    imc.save = save;
-    imc.owner = owner;
+    omc.cancel = cancel;
+    omc.save = save;
+    omc.owner = owner;
+    omc.validateUser = validateUser;
 
 
     function save(){
-      $uibModalInstance.close(imc.owner);
+      $uibModalInstance.close(omc.owner);
     }
 
     function cancel() {
       $uibModalInstance.dismiss();
+    }
+
+    function validateUser() {
+      if (omc.stateForm.$valid) {
+        omc.stateForm.$setUntouched();
+        save();
+
+      } else {
+        touchControlls();
+      }
+    }
+
+    function touchControlls() {
+      //Prođi kroz sve propertie stateForm objekta
+      angular.forEach(omc.stateForm, function(value, key) {
+        //Pronađi propertie čiji naziv počinje sa "input"
+        if (key.indexOf("input") === 0) {
+          //"Dodirni" polje
+          value.$setTouched();
+        }
+      });
     }
 
   }
