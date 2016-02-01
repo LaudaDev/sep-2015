7ebe254a259b6cdeeb952766d3ec655ec3488805
Zorana Deuric <zdeuric@gmail.com>
2016-02-01 21:03:19 +0100

    Data binding errors

--- backend/rest/MerchantRest/src/main/java/app/controllers/PaymentController.java
+++ backend/rest/MerchantRest/src/main/java/app/controllers/PaymentController.java
@@ -2,13 +2,13 @@ package app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import app.model.Insurance;
 import app.model.transferData.PaymentInstructionsAcquirerResponse;
@@ -25,11 +25,10 @@ public class PaymentController {
 	
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public PaymentInstructionsAcquirerResponse buyInsurance(@Validated @RequestBody Insurance insurance, RedirectAttributes redirectAttributes)
+	public PaymentInstructionsAcquirerResponse buyInsurance(@Validated @RequestBody Insurance insurance)
 	{	
 		PaymentInstructionsAcquirerResponse instructions = paymentService.buyInsurance(insurance);
-		
-	//	redirectAttributes.addFlashAttribute("paymentID", instructions.getPaymentID());
+
 		return instructions;
 
 	
@@ -38,9 +37,9 @@ public class PaymentController {
 	
 	
 	@RequestMapping(value = "transactionResults",method = RequestMethod.POST)
-	public ResponseEntity<?> receiveTransactionResults(@Validated @RequestBody TransactionResults transactionResults)
+	public ResponseEntity<?> receiveTransactionResults(@Validated @RequestBody TransactionResults transactionResults, BindingResult bindingResult)
 	{	
-		return paymentService.saveTransactionResult(transactionResults);	
+		return paymentService.saveTransactionResult(transactionResults, bindingResult);	
 	}
 	
 	
