62f631d95c3e91f80762f11ecea434f8076d7212
Zorana Deuric <zdeuric@gmail.com>
2016-02-01 21:06:00 +0100

    Error URL for invalid data

--- backend/rest/MerchantRest/src/main/java/app/services/PaymentService.java
+++ backend/rest/MerchantRest/src/main/java/app/services/PaymentService.java
@@ -12,17 +12,22 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import app.commons.Consts;
 import app.commons.CustomIdGenerator;
+import app.commons.Messages;
+import app.commons.TransactionStatusCodes;
+import app.commons.UrlRegister;
 import app.model.Insurance;
 import app.model.Transaction;
 import app.model.transferData.MerchantInfo;
 import app.model.transferData.MerchantPaymentRequest;
 import app.model.transferData.PaymentInstructionsAcquirerResponse;
 import app.model.transferData.TransactionResults;
+import app.model.transferData.TransactionStatus;
 import app.services.exceptions.CustomRestClientException;
 
 @Service
@@ -40,6 +45,7 @@ public class PaymentService {
 	public PaymentInstructionsAcquirerResponse buyInsurance(Insurance insurance) {
 
 		Insurance newInsurance = (Insurance) insuranceService.create(insurance).get("insurance");
+
 		MerchantPaymentRequest paymentRequest = createPaymentRequest(newInsurance.getAmountToPay());
 
 		PaymentInstructionsAcquirerResponse instructions = getPaymentInstructions(paymentRequest);
@@ -50,71 +56,85 @@ public class PaymentService {
 
 		logger.info("PaymentInstructions received");
 
-		Transaction transaction = createTransaction(paymentRequest.getMerchantInfo(), newInsurance.getId(), instructions.getPaymentID());
+		Transaction transaction = createTransaction(paymentRequest.getMerchantInfo(), newInsurance.getId(),
+				instructions.getPaymentID());
 
 		transactionService.save(transaction);
 		return instructions;
 	}
 
-	public ResponseEntity<?> saveTransactionResult(TransactionResults transactionResults) {
+	public ResponseEntity<?> saveTransactionResult(TransactionResults transactionResults, BindingResult bindingResult) {
 
 		URL resultURL = null;
 		String userMessage = "";
 
-		Transaction transaction = transactionService.findByMerchantOrderAndPaymentId(transactionResults.getMerchantOrderId(), transactionResults.getPaymentId());
-		transaction.setAcquirerInfo(transactionResults.getAcquirerInfo());
-		transaction.setTransactionStatus(transactionResults.getTransactionStatus());
-	
-		if (transaction.getTransactionStatus().getCode().equals(Consts.SUCCESS_TRANSACTION_CODE)) {
+		if (bindingResult.hasErrors()) {
 
-			resultURL = Consts.SUCCESS_URL;
-			userMessage = Consts.SUCCESS_MESSAGE;
+			resultURL = UrlRegister.ERROR_URL;
+
 		} else {
+			
+			TransactionStatus status = transactionResults.getTransactionStatus();
+			
+			if (status.getCode().equals(TransactionStatusCodes.SUCCESS.getValue())) {
 
-			resultURL = Consts.FAILED_URL;
-			userMessage = Consts.CARD_PROBLEM_MESSAGE;
+				resultURL = UrlRegister.SUCCESS_URL;
+				userMessage = Messages.SUCCESS_MESSAGE;
 
-		}
-		logger.info("Issuer response "+transaction.getTransactionStatus().getMessage());
+			} else if (status.getCode().equals(TransactionStatusCodes.SERVER_ERROR.getValue())) {
+
+				resultURL = UrlRegister.ERROR_URL;
+				userMessage = Messages.SERVER_PROBLEM_MESSAGE;
+
+			} else {
+
+				resultURL = UrlRegister.FAILED_URL;
+				userMessage = Messages.CARD_PROBLEM_MESSAGE;
+
+			}
+
+			Transaction transaction = transactionService.findByMerchantOrderAndPaymentId(
+					transactionResults.getMerchantOrderId(), transactionResults.getPaymentId());
+
+			transaction.setAcquirerInfo(transactionResults.getAcquirerInfo());
+			transaction.setTransactionStatus(transactionResults.getTransactionStatus());
 		
+			transactionService.update(transaction);
 
-		transactionService.update(transaction);
+			if (transaction.getInsuranceId() != null) {
+				Map<String, String> carrierMailInfo = insuranceService
+						.getCarrierEmailInfo(transaction.getInsuranceId());
+				String carrierEmail = carrierMailInfo.get("email");
+				String carrierNameAndSurname = carrierMailInfo.get("name") + " " + carrierMailInfo.get("surname");
+				if (carrierEmail != null) {
+
+					mailService.send(userMessage, carrierEmail, carrierNameAndSurname);
+				} else {
+					logger.info("Carrier mail is null.");
+				}
+			} else {
+				logger.error("Insurance id is null,  transaction with id " + transaction.getId());
+			}
+		}
 
 		HttpHeaders headers = new HttpHeaders();
 		try {
+
 			headers.setLocation(resultURL.toURI());
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		if(transaction.getInsuranceId()!=null)
-		{
-			Map<String, String> carrierMailInfo =insuranceService.getCarrierEmailInfo(transaction.getInsuranceId());
-			String carrierEmail = carrierMailInfo.get("email");
-			String carrierNameAndSurname = carrierMailInfo.get("name")+carrierMailInfo.get("surname");
-			if(carrierEmail != null){
-				
-				mailService.send(userMessage, carrierEmail, carrierNameAndSurname);
-			}
-			else
-			{
-				logger.info("Carrier mail is null.");
-			}
-		}else
-		{
-			logger.error("Insurance id in transaction with id "+transaction.getId()+" is null");
-		}
-		
 		return new ResponseEntity<>(headers, HttpStatus.CREATED);
 
 	}
 
 	public PaymentInstructionsAcquirerResponse getPaymentInstructions(MerchantPaymentRequest request) {
-		PaymentInstructionsAcquirerResponse instructions = null;
 
+		PaymentInstructionsAcquirerResponse instructions = null;
 		try {
-			instructions = restTemplate.postForObject(Consts.ACQUIRER_URL.toString(), request,
+			instructions = restTemplate.postForObject(UrlRegister.ACQUIRER_URL.toString(), request,
 					PaymentInstructionsAcquirerResponse.class);
 		} catch (RestClientException e) {
 			logger.error(e.getStackTrace().toString());
@@ -128,15 +148,14 @@ public class PaymentService {
 		int transactionId = CustomIdGenerator.getTransactionId();
 		Date timestamp = new Date();
 		MerchantPaymentRequest newPaymentRequest = new MerchantPaymentRequest(Consts.MERCHANT_ID,
-				Consts.MERCHANT_PASSWORD, amount,new MerchantInfo(transactionId,timestamp), Consts.ERROR_URL);
+				Consts.MERCHANT_PASSWORD, amount, new MerchantInfo(transactionId, timestamp), UrlRegister.ERROR_URL);
 		logger.info("Payment request successfully created");
 		logger.info(newPaymentRequest.toString());
 		return newPaymentRequest;
 
 	}
 
-	public Transaction createTransaction(MerchantInfo merchantInfo, String insuranceId,
-			int paymentID) {
+	public Transaction createTransaction(MerchantInfo merchantInfo, String insuranceId, int paymentID) {
 		Transaction transaction = new Transaction();
 		transaction.setMerchantInfo(merchantInfo);
 		transaction.setInsuranceId(insuranceId);
