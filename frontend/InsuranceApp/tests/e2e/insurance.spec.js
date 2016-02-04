51138555298137b20a964c35ab8c6b0444f3f1e8
jovicazaric <zaric.jovica.zaric@gmail.com>
2016-02-05 00:34:29 +0100

    Added test for checking if add insured and add carrirer buttons are disabled and when (based on inserting persons)

--- frontend/InsuranceApp/tests/e2e/insurance.spec.js
+++ frontend/InsuranceApp/tests/e2e/insurance.spec.js
@@ -78,7 +78,7 @@ describe("Testing insurance basic info form", function(){
 		browser.sleep(browserPause * 2);	
 		
 		price = element(by.id("priceLabel")).getText();
-		expect(price).toBe("3.155,32 dins");
+		expect(price).toBe("3.155,32 din");
 		
 	});
 	
@@ -94,5 +94,97 @@ describe("Testing insurance basic info form", function(){
 		browser.sleep(browserPause * 2);
 		
 	});
+});
+
+describe("Testing adding persons details", function(){
+	var InsuranceBasicForm = function(){
+		this.days = element(by.model("ic.insurance.travel.duration"));
+		this.less = element(by.model("ic.insurance.travel.less"));
+		this.between = element(by.model("ic.insurance.travel.between"));
+		this.over = element(by.model("ic.insurance.travel.over"));
+		this.regions = element(by.model('ic.insurance.travel.region')).all(by.tagName('option'));
+		this.amount = element(by.model('ic.insurance.travel.insuredAmount')).all(by.tagName('option'));
+		this.doesSport = element(by.model("ic.insurance.travel.doesSport"));
+	};
+	
+	var AddInsuredForm = function(){
+		this.name = element(by.model("imc.user.name"));
+		this.surname = element(by.model("imc.user.surname"));
+		this.jmbg = element(by.model("imc.user.jmbg"));
+		this.passport = element(by.model("imc.user.passportNum"));
+		this.address = element(by.model("imc.user.address"));
+		this.phone = element(by.model("imc.user.phoneNum"));
+	};
+	
+	beforeEach(function(){
+		browser.driver.manage().window().setSize(1280, 800);
+		browser.get("/#/insurance");
+	});
+	
+	var browserPause = 500;
+	
+	it("Check persons details form", function(){
+		var basicForm = new InsuranceBasicForm();
+		
+		basicForm.days.sendKeys("7");
+		basicForm.regions.get(2).click();		
+		basicForm.between.sendKeys("2");		
+		basicForm.amount.get(4).click();
+		
+		var next = element(by.buttonText("Next"));
+		next.click();
+		
+		expect(browser.getCurrentUrl()).toContain("insuranceDetails");
+		var addInsured = element(by.id("addInsured"));
+		expect(addInsured.getAttribute("disabled")).toEqual(null);
+		var addCarrier = element(by.id("addCarrier"));
+		expect(addCarrier.getAttribute("disabled")).toEqual("true");
+		
+		addInsured.click();
+		browser.sleep(browserPause);
+		
+		var addInsured1 = new AddInsuredForm();
+		addInsured1.name.sendKeys("Pera");
+		browser.sleep(browserPause);
+		addInsured1.surname.sendKeys("Peric");
+		browser.sleep(browserPause);
+		addInsured1.jmbg.sendKeys("0306992180740");
+		browser.sleep(browserPause);
+		addInsured1.passport.sendKeys("123456789");
+		browser.sleep(browserPause);
+		addInsured1.address.sendKeys("Perina adresa");
+		browser.sleep(browserPause);
+		addInsured1.phone.sendKeys("065336663");
+		browser.sleep(browserPause);
+		
+		var saveInsured = element(by.id("saveInsured"));
+		saveInsured.click();
+		
+		expect(addInsured.getAttribute("disabled")).toEqual(null);
+		expect(addCarrier.getAttribute("disabled")).toEqual("true");
+		browser.sleep(browserPause * 3);
+		
+		addInsured.click();
+		
+		var addInsured2 = new AddInsuredForm();
+		addInsured1.name.sendKeys("Zika");
+		browser.sleep(browserPause);
+		addInsured1.surname.sendKeys("Zikic");
+		browser.sleep(browserPause);
+		addInsured1.jmbg.sendKeys("0306992180783");
+		browser.sleep(browserPause);
+		addInsured1.passport.sendKeys("987654321");
+		browser.sleep(browserPause);
+		addInsured1.address.sendKeys("Zikina adresa");
+		browser.sleep(browserPause);
+		addInsured1.phone.sendKeys("065222000");
+		browser.sleep(browserPause);
+		
+		saveInsured.click();
+		expect(addInsured.getAttribute("disabled")).toEqual("true");
+		expect(addCarrier.getAttribute("disabled")).toEqual(null);
+		browser.sleep(browserPause * 3);		
+	});
+	
 
 });
